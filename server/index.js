4110d48c532c4fc9c10070103b7e5b124d4a9939
Ethan Fung <EthanEFung@gmail.com>
2018-01-22 14:11:53 -0800

    chore: configure express server

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const { load, config } = require('dotenv');
+load();
+config();
+
+const express = require('express');
+const path = require('path');
+const { urlencoded, json } = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 8080;
+const router = require('./routes/index');
+
+app
+  .use(cors())
+  .use(urlencoded({ extended: true }))
+  .use(json())
+  .use(router);
+
+app.listen(port, err => {
+  if (err) throw `could not connect to port ${port}`;
+  console.log(`listening on port ${port}`);
+});  
